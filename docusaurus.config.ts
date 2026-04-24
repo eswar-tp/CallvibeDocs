6310c9e0e2a0b057716f4ff2c2a048d1c5bcfd13
Yomi <yomis@techprofuse.com>
2026-04-24 19:06:30 +0530

    Remove social card meta image from docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,8 +61,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       defaultMode: 'light',
       respectPrefersColorScheme: false,
